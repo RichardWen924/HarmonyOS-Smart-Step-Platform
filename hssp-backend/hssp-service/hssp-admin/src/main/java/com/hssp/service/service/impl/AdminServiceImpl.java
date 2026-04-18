6ab7c76ca560d6dc6348624086c60eca8ff9b363
Sail <2184987541@qq.com>
2026-04-18 20:35:46 +0800

    同步vue端与后端管理端登录逻辑

--- hssp-backend/hssp-service/hssp-admin/src/main/java/com/hssp/service/service/impl/AdminServiceImpl.java
+++ hssp-backend/hssp-service/hssp-admin/src/main/java/com/hssp/service/service/impl/AdminServiceImpl.java
@@ -63,66 +63,101 @@ public class AdminServiceImpl extends ServiceImpl<AdminMapper,Admin> implements 
         }
     }
 
+    /*
+     * 管理员登录
+     * @param loginDto 登录参数,目前支持用户名/邮箱加密码登录
+     * @return 登录成功返回令牌，失败返回null
+     */
     @Override
     public String login(LoginDto loginDto) {
-        String Username = loginDto.getUsername();
+        String input = loginDto.getUsername();
         String password = loginDto.getPassword();
-        String email = loginDto.getEmail();
-        String verification = loginDto.getVerification();
-        //验证码不为空，即选择了邮箱加验证码的登录方式
-        if(verification!=null){
-            if(email==null){
-                throw new BusinessException("邮箱不能为空");
-            }
-            log.info("邮箱+验证码登录");
-            String cacheVerification = Objects.requireNonNull(redisTemplate.opsForValue().get(CODE_PREFIX + email)).toString();
-            if(!cacheVerification.equals(verification)){
-                throw new BusinessException("验证码错误");
-            }
-            Admin admin=lambdaQuery()
-                    .eq(Admin::getEmail, email)
-                    .one();
-            if(admin==null){
-                throw new BusinessException("邮箱不存在");
-            }
-            // 登录成功，生成令牌
-            String token= JwtUtils.createToken(admin.getId());
-            return token;
-        }
-        //验证码为空，即选择了用户名/邮箱加密码的登录方式
-        else{
-            if(Username!=null){
-                String encodePassword = passwordEncoder.encode(password);
-                log.info("用户名+密码登录");
-                Admin admin=lambdaQuery()
-                        .eq(Admin::getUsername, Username)
-                        .one();
-                boolean rightPassword=passwordEncoder.matches(password, admin.getPassword());
-                if(!rightPassword){
-                    throw new BusinessException("用户名或密码错误");
-                }
-                // 登录成功，生成令牌
-                String token= JwtUtils.createToken(admin.getId());
-                return token;
-            }
-            else if(email!=null){
-                String encodePassword = passwordEncoder.encode(password);
-                log.info("邮箱+密码登录");
-                Admin admin=lambdaQuery()
-                        .eq(Admin::getEmail, email)
-                        .one();
-                boolean rightPassword=passwordEncoder.matches(password, admin.getPassword());
-                if(!rightPassword){
-                    throw new BusinessException("用户名或密码错误");
-                }
-                // 登录成功，生成令牌
-                String token= JwtUtils.createToken(admin.getId());
-                return token;
-            }
-            else{
-                throw new BusinessException("用户名/邮箱不能为空");
-            }
 
+        if (Objects.isNull(password)) {
+            throw new BusinessException("密码不能为空");
         }
+
+        if (Objects.isNull(input)) {
+            throw new BusinessException("邮箱/用户名不能为空");
+        }
+
+        Admin admin = lambdaQuery()
+                .eq(Admin::getUsername, input)
+                .or()
+                .eq(Admin::getEmail, input)
+                .one();
+        if (Objects.isNull(admin)) {
+            throw new BusinessException("用户名/邮箱不存在");
+        }
+
+        boolean rightPassword = passwordEncoder.matches(password, admin.getPassword());
+        if (!rightPassword) {
+            throw new BusinessException("用户名或密码错误");
+        }
+        // 登录成功，生成令牌
+        return JwtUtils.createToken(admin.getId());
     }
+
+//    @Override
+//    public String login(LoginDto loginDto) {
+//        String Username = loginDto.getUsername();
+//        String password = loginDto.getPassword();
+//        String email = loginDto.getEmail();
+//        String verification = loginDto.getVerification();
+//        //验证码不为空，即选择了邮箱加验证码的登录方式
+//        if(verification!=null){
+//            if(email==null){
+//                throw new BusinessException("邮箱不能为空");
+//            }
+//            log.info("邮箱+验证码登录");
+//            String cacheVerification = Objects.requireNonNull(redisTemplate.opsForValue().get(CODE_PREFIX + email)).toString();
+//            if(!cacheVerification.equals(verification)){
+//                throw new BusinessException("验证码错误");
+//            }
+//            Admin admin=lambdaQuery()
+//                    .eq(Admin::getEmail, email)
+//                    .one();
+//            if(admin==null){
+//                throw new BusinessException("邮箱不存在");
+//            }
+//            // 登录成功，生成令牌
+//            String token= JwtUtils.createToken(admin.getId());
+//            return token;
+//        }
+//        //验证码为空，即选择了用户名/邮箱加密码的登录方式
+//        else{
+//            if(Username!=null){
+//                String encodePassword = passwordEncoder.encode(password);
+//                log.info("用户名+密码登录");
+//                Admin admin=lambdaQuery()
+//                        .eq(Admin::getUsername, Username)
+//                        .one();
+//                boolean rightPassword=passwordEncoder.matches(password, admin.getPassword());
+//                if(!rightPassword){
+//                    throw new BusinessException("用户名或密码错误");
+//                }
+//                // 登录成功，生成令牌
+//                String token= JwtUtils.createToken(admin.getId());
+//                return token;
+//            }
+//            else if(email!=null){
+//                String encodePassword = passwordEncoder.encode(password);
+//                log.info("邮箱+密码登录");
+//                Admin admin=lambdaQuery()
+//                        .eq(Admin::getEmail, email)
+//                        .one();
+//                boolean rightPassword=passwordEncoder.matches(password, admin.getPassword());
+//                if(!rightPassword){
+//                    throw new BusinessException("用户名或密码错误");
+//                }
+//                // 登录成功，生成令牌
+//                String token= JwtUtils.createToken(admin.getId());
+//                return token;
+//            }
+//            else{
+//                throw new BusinessException("用户名/邮箱不能为空");
+//            }
+//
+//        }
+//    }
 }
